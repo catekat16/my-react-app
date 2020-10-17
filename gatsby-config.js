b92047632334d794318d3172e51d27022d872d20
Caitlyn Chen <caitlyn.chen8@gmail.com>
2020-10-17 14:42:13 -0400

    watch button text

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Mojo Space',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,11 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Mojo x Lumos collab</h1>
+    <p>
+      The gen z post event networking space. Sign up today for early access.
+    </p>
+    <Link to="/page-2/">Watch the video</Link>
   </div>
 )
 
